0298072fc5c542bc66de5f15f73e602d2530f25c
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-10-19 16:49:31 +0200

    update push

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,48 +1,44 @@
-import React from 'react';
-import Contact from './Contact'
+import React from "react";
+import Contact from "./Contact";
 
+const itemList = [
+  {
+    image: "https://randomuser.me/api/portraits/women/8.jpg",
+    name: "Eleanor Ferguson"
+  },
 
-    const itemList = [
-        {
-        image: "https://randomuser.me/api/portraits/women/8.jpg",
-        name: "Eleanor Ferguson",
-          
-        },
+  {
+    image: "https://randomuser.me/api/portraits/men/80.jpg",
+    name: "Shane Henderson"
+  },
 
-        {
-        image: 'https://randomuser.me/api/portraits/men/80.jpg',
-        name: 'Shane Henderson',
-        
-        },
+  {
+    image: "https://randomuser.me/api/portraits/women/52.jpg",
+    name: "Samantha Kuhn"
+  },
 
+  {
+    image: "https://randomuser.me/api/portraits/women/40.jpg",
+    name: "Kaylee Prescott"
+  },
 
-        {
-        image: 'https://randomuser.me/api/portraits/women/52.jpg',
-        name: 'Samantha Kuhn',
-         
-        },
-
-        {
-        image: 'https://randomuser.me/api/portraits/women/40.jpg',
-        name: 'Kaylee Prescott',
-        
-        },
-
-        {
-        image: 'https://randomuser.me/api/portraits/men/56.jpg',
-        name: 'Jerem Valles',
-        },
-        ]  
-
-
+  {
+    image: "https://randomuser.me/api/portraits/men/56.jpg",
+    name: "Jerem Valles"
+  }
+];
 
 const ContactList = () => (
-    <div>
-    {itemList.map((item,i) =>(
-        <Contact key={i} image={item.image} name={item.name} online={item.online} />
+  <div>
+    {itemList.map((item, i) => (
+      <Contact
+        key={i}
+        image={item.image}
+        name={item.name}
+        online={item.online}
+      />
     ))}
-    </div>
+  </div>
 );
-
 
 export default ContactList;
